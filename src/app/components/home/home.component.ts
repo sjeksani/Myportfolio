855b6bf6ab80d2b1213543549381045278260e7b
Sharath Jeksani <admin@Admins-MacBook-Pro.local>
2019-01-24 15:32:22 -0800

    adding files

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,7 +7,34 @@ styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent {
+
+    items: Array<any> = []
+    
     constructor() {
 
+        this.items = [
+            { name: 'assets/images/mypicture.jpg' },
+            { name: 'assets/images/img.jpeg' }
+          ]
+        }
+
     }
-}
+
+    // carousel() {
+
+    //     let x = (<HTMLElement[]><any>document.getElementsByClassName('mySlides'))[0];
+
+    //     // let x : Array<HTMLElement> = [];
+    //     // x = document.getElementsByClassName('mySlides');
+    //     for (let i = 0; i < x.length; i++) {
+    //     x[i].style.display = "none"; 
+    // }
+    // this.slideIndex++;
+    // if (this.slideIndex > x.length) {
+    //     this.slideIndex = 1
+    // } 
+    // x[this.slideIndex-1].style.display = "block"; 
+    // setTimeout(this.carousel, 2000); 
+
+    // }
+
